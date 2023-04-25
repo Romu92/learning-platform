0af86f1fc962e01bdf48f7757b0a2f45c6558a81
Romualdo Mastromatteo <romu.92@hotmail.it>
2023-04-25 17:53:26 +0200

    first commit

---
+++ .sql/drop.sql
@@ -0,0 +1,8 @@
+-- drop table courses_students;
+-- -- la tabella per le relazioni va cancellata prima
+
+-- drop table lessons;
+
+-- drop table students;
+
+drop table courses;
---
+++ .sql/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO courses(
+    name,
+    description,
+    teacher_firstname,
+    teacher_lastname,
+    date
+)
+values(
+    'Hack java 01',
+    'il corso più interessante',
+    'Andrea',
+    'Mininni',
+    '2023-05-22'
+);
